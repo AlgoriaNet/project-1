3d68322114f5c73821f61b95fcb42427495dfe43
yangdegui-cd <your_email@example.com>
2024-12-17 16:20:54 +0800

    添加: 添加DOTween插件, 用于UI的简单动画效果

--- Assets/Scripts/UI Controller/ProfileController.cs
+++ Assets/Scripts/UI Controller/ProfileController.cs
@@ -10,10 +10,12 @@ namespace UI_Controller
         PlayerWebSocketApi _playerApi;
         private void Awake()
         {
+            //临时的建立连接,  正常流程是在登录成功后建立连接
             WebSocketManager.Instance.ConnectWebSocket();
             _playerApi = PlayerWebSocketApi.Instance;
         }
 
+        //临时的获取玩家信息,  正常流程是在登录成功后获取
         private void Start() => _playerApi.Action("profile", new { }, SetProfileFromServer);
 
         private void SetProfileFromServer(JObject obj) => PlayerProfile.Data.SetPlayer(obj["Player"].ToObject<Player>());
--- Assets/Scripts/UI Scripts/ToggleIconGroup.cs
+++ Assets/Scripts/UI Scripts/ToggleIconGroup.cs
@@ -1,16 +1,71 @@
+using System;
 using UnityEngine;
+using DG.Tweening;
 
 public class ToggleIconGroup : MonoBehaviour
 {
     public GameObject buttonGroup; // Assign Icon Group in the Inspector
-
+    public float duration = 0.5f;
+    
+    private RectTransform _rectTransform;
+    private Vector2 _originSizeDelta;
     private bool isVisible = true;
 
-    public void ToggleVisibility()
+    private void Start()
     {
-        isVisible = !isVisible;
+        _rectTransform = buttonGroup.GetComponent<RectTransform>();
+    }
 
+    public void ToggleVisibilityToBottom()
+    {
         // Show or hide the Icon Group
-        buttonGroup.SetActive(isVisible);
+        if (isVisible)
+        {
+            _originSizeDelta = _rectTransform.sizeDelta;
+            FadeFromTopToBottom();
+        }
+        else
+        {
+            AppearFromBottomToTop();
+        }
+        isVisible = !isVisible;
+    }
+
+    public void ToggleVisibilityToTop()
+    {
+        // Show or hide the Icon Group
+        if (isVisible)
+        {
+            _originSizeDelta = _rectTransform.sizeDelta;
+            FadeFromBottomToTop();
+        }
+        else
+        {
+            AppearFromTopToBottom();
+        }
+        
+        isVisible = !isVisible;
+    }
+
+    private void FadeFromTopToBottom()
+    {
+        _rectTransform.DOSizeDelta(new Vector2(_originSizeDelta.x, 0), duration);
+        _rectTransform.DOAnchorPosY(- _originSizeDelta.y, duration);
+    }
+
+    private void AppearFromBottomToTop()
+    {
+        _rectTransform.DOSizeDelta(_originSizeDelta, duration);
+        _rectTransform.DOAnchorPosY(0, duration);
+    }
+
+    private void FadeFromBottomToTop()
+    {
+        _rectTransform.DOSizeDelta(new Vector2(_originSizeDelta.x, 0), duration);
+    }
+
+    private void AppearFromTopToBottom()
+    {
+        _rectTransform.DOSizeDelta(_originSizeDelta, duration);
     }
 }
